371180765fdd07f0ae9fefeb876cc46f03464c72
Mustakim Khalifa <mustakim.khalifa@1rivet.local>
2020-02-11 11:43:32 +0530

    Add : Added Demo MVC

---
+++ DemoMVC/DemoMVC/SecondCustomeMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoMVC
+{
+    public class SecondCustomeMiddleware
+    {
+        public static void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                // Do work that doesn't write to the Response.
+                await next.Invoke();
+                // Do logging or other work that doesn't write to the Response.
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from Second Custome Middleware.");
+            });
+        }
+    }
+}
